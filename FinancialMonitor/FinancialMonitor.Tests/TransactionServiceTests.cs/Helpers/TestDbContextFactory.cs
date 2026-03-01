e81cb2269f8a472f95eb3da64fa749fee99677be
Shevi Kukis <shevi3151@gmail.com>
2026-03-01 14:57:34 +0200

    Switch tests to SQLite InMemory for realistic DB behavior

--- FinancialMonitor/FinancialMonitor.Tests/TransactionServiceTests.cs/Helpers/TestDbContextFactory.cs
+++ FinancialMonitor/FinancialMonitor.Tests/TransactionServiceTests.cs/Helpers/TestDbContextFactory.cs
@@ -1,4 +1,5 @@
 ﻿using FinancialMonitor.Data;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinancialMonitor.Tests.Helpers;
@@ -7,10 +8,17 @@ public static class TestDbContextFactory
 {
     public static AppDbContext Create()
     {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open(); 
+
         var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .UseSqlite(connection)
             .Options;
 
-        return new AppDbContext(options);
+        var context = new AppDbContext(options);
+
+        context.Database.EnsureCreated(); 
+
+        return context;
     }
 }
